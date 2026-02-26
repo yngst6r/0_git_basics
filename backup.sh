cbf85e01024e315cc1b99d2dc1b2d1c0064b38e4
yngst6r <zhtheodor@gmail.com>
2026-02-26 14:45:36 +0300

    Add backup.sh script

---
+++ backup.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+# Backup script for lab work
+
+DATE=$(date +%Y%m%d_%H%M%S)
+SOURCE_DIR="."
+BACKUP_FILE="backup_${DATE}.tar.gz"
+
+echo "Creating backup: $BACKUP_FILE"
+tar -czf "$BACKUP_FILE" "$SOURCE_DIR" --exclude="*.tar.gz"
+echo "Backup completed: $BACKUP_FILE"
